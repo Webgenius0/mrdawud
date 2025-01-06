6044593d2332b3fb2f91016a1a70380aa2b71e72
MuhammadSagor <sagorwdpf@gmail.com>
2025-01-06 09:15:10 +0600

    helper

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -20,4 +20,22 @@ class Helper
         return $path;
     }
 
+    //video upload
+    public static function videoUpload($file, $folder, $name)
+    {
+        $videoName = Str::slug($name) . '.' . $file->extension();
+        $file->move(public_path('uploads/' . $folder), $videoName);
+        $path = 'uploads/' . $folder . '/' . $videoName;
+        return $path;
+    }
+
+    // audio upload
+    public static function audioUpload($file, $folder, $name)
+    {
+        $audioName = Str::slug($name) . '.' . $file->extension();
+        $file->move(public_path('uploads/' . $folder), $audioName);
+        $path = 'uploads/' . $folder . '/' . $audioName;
+        return $path;
+    }
+
 }
