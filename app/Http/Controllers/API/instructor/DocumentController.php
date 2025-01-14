d92d8733036dc272b3b3378f7601171c29fa3337
MuhammadSagor <sagorwdpf@gmail.com>
2025-01-14 10:21:50 +0600

    update documents

--- app/Http/Controllers/API/instructor/DocumentController.php
+++ app/Http/Controllers/API/instructor/DocumentController.php
@@ -8,28 +8,5 @@ use Illuminate\Support\Facades\Validator;
 
 class DocumentController extends Controller
 {
-    public function index(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'document'=>'required|file|mimes:pdf,doc,docx',
-        ]);
 
-        if($validator->fails())
-        {
-            return response()->json(['errors' => $validator->errors()], 400);
-        }
-
-        $user= auth()->user();
-        if(!$user)
-        {
-            return response()->json(['message'=>'User not found.'],404);
-        }
-
-        $document = new Document();
-        $document->user_id= $user->id;
-        if$request->hasFile('document')
-        {
-            
-        }
-    }
 }
